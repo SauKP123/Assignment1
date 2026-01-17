065ac54288294484d385c6cefb44d15157208575
SauKP123 <saurabhkrp123@gmail.com>
2026-01-17 13:40:36 +0530

    Add files via upload

---
+++ task_2_Personalized_greeting.py
@@ -0,0 +1,14 @@
+"""
+Takes a user's first and last name as input
+Concatenates the first and last name into a full name
+Prints a personalized greeting message using the full name
+"""
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+Name = first_name + " " + last_name
+
+print("Hello," + Name + "!"  " Welcome to the python program.")
+
+
