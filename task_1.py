29b3f810bf7e7db695cde43ee7075fee37719168
SauKP123 <saurabhkrp123@gmail.com>
2026-01-17 13:50:46 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,19 @@
+"""
+Takes two numbers as input from the user- a,b
+Perform the basic mathematical operations on these two numbers
+Addition = a + b
+Subtraction = a - b
+Multiplication = a * b
+Division = a / b
+"""
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+Addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print("Addition: ", Addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
